c16b960c3a792f9e8d15270bf0513fe502c5a3ab
Benjamin <benjamin.roelandt@gmail.com>
2018-05-27 18:44:34 +0200

    Modif gg fest

--- Source/IslandEscape/IslandEscapeGameMode.cpp
+++ Source/IslandEscape/IslandEscapeGameMode.cpp
@@ -6,10 +6,5 @@
 
 AIslandEscapeGameMode::AIslandEscapeGameMode()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/SideScrollerCPP/Blueprints/SideScrollerCharacter"));
-	if (PlayerPawnBPClass.Class != NULL)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+
 }
